696314e38ca0f43efaa9e5d9d932270eccd634e7
Emilychaparro <ebohorquez526@gmail.com>
2023-04-12 18:44:50 -0500

    Taller 2

---
+++ programa.js
@@ -0,0 +1,23 @@
+
+//comentario de una linea
+/*
+comentarios de
+varias lineas
+documentar el codigo
+*/
+
+//No es necesario declarar una variable antes de ultilizar
+//Variable:Un lugar en la memoria al cual coloco el nombre ,
+// y lo uso para guardar un valor 
+
+
+//variables de ambito de funcion 
+var a;
+
+//variable de ambito de bloque
+let b ; 
+
+//operaciones;arimetricas,comparacion, logica binaria (AND, OR)
+
+//Estructura de datos : arreglados..guardan mas de una variable
+
